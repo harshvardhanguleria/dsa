d46b9f4a3735a034dbb05b4ff7da03c47c60427a
hvg100 <harshvardhanguleria@gmail.com>
2021-12-04 21:05:34 +0530

    Modified

---
+++ DLL.py
@@ -0,0 +1,32 @@
+# !/usr/bin/python3
+
+# Doubly Linked List
+
+# Node class
+class Node:
+    # constructor to initialize a node
+    def __init(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+    
+# Doubly Linked List class
+class DLL:
+    # constructor to initialize head node
+    def __init(self):
+        self.head = None
+
+# Function to print action menu
+def printMenu():
+    print("\n1. Insert into the linked list\n"+
+            "2. Delete node from the linked list\n"+
+            "3. View the linked list\n"+
+            "4. Reverse the linked list\n"+
+            "5. Exit")
+
+# Main function to enable the functionality of the code
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
