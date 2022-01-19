7e585be771c768f9e0566820050754439f5aebd9
khal-drogo-git <harshvardhanguleria@gmail.com>
2022-01-19 13:47:16 +0530

    Modified

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -112,8 +112,38 @@ class DLL {
         positionNode.prev.next = positionNode.next;
         positionNode = null;
     }
+
+    // Reverse the linked list
+    public void reverse() {
+        
+    }
 }
 
 public class DoublyLinkedList {
+
+    private static void menu(int choice) {
+        if (choice == 1) {
+            System.out.print("\n1. Insert into the linked list"+
+                                "\n2. Delete node from the linked list"+
+                                "\n3. View the linked list"+
+                                "\n4. Reverse the linked list"+
+                                "\n5. Exit"+
+                                "\nEnter your choice: ");
+        } else if (choice == 2) {
+            System.out.print("\n1. Insert in the end"+
+                                "\n2. Insert in the beginning"+
+                                "\n3. Insert at a specific position"+
+                                "\nEnter your choice: ");
+        } else if (choice == 3) {
+            System.out.print("\n1. Delete at the end"+
+                                "\n2. Delete from the beginning"+
+                                "\n3. Delete from a specific position"+
+                                "\nEnter your choice: ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
     
 }
