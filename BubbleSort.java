9c8ef0dd856b2bdfc191fea91ef96952a05f9446
khal-drogo-git <harshvardhanguleria@gmail.com>
2022-01-22 16:01:02 +0530

    Modified

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,5 +1,32 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class BubbleSort {
+
+    private static ArrayList<Integer> list = new ArrayList<>();
+
+    private static void display() {
+        for (Integer a : list) {
+            if (a == -1)
+                break;
+            System.out.println(a);
+        }
+    }
+
+    private static void bubbleSort() {
         
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        while (a != -1) {
+            System.out.println("\nEnter -1 to stop entering numbers into the array list.");
+            a = sc.nextInt();
+            list.add(a);
+        }
+        display();
+        //bubbleSort();
+        //display();
+    }
 }
