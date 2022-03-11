99a59da0cccbf6a7c4ec20fc5e8962f0f192ac04
khal-drog0 <harshvardhanguleria@gmail.com>
2022-03-11 16:24:52 +0530

    Modified

--- BFS.java
+++ BFS.java
@@ -1,65 +1,30 @@
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-
-class Node {
-    String data;
-    Node left;
-    Node right;
-
-    Node(String data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-class Tree {
-    private Node root;
-
-    Tree() {
-        this.root = null;
-    }
-
-    public void insertNode() {
-        Scanner sc = new Scanner(System.in);
-        Node newNode;
-        Node leftNode;
-        Node rightNode;
-        Node temp;
-        char ch;
-        String data;
-        do {
-            System.out.print("\nEnter the data to be inserted: ");
-            data = sc.nextLine();
-            temp = this.root;
-            newNode = new Node(data);
-            if (this.root == null) 
-                this.root = newNode;
-            else {
-                while (temp != null) {
-
-                }
-            }
-            System.out.print("\nDo you want to enter more(y/n): ");
-            ch = sc.next().charAt(0);
-        } while (ch == 'y' || ch == 'Y');
-    }
-}
+import java.util.LinkedList;
 
 public class BFS {
 
-    private static List<String> list = new ArrayList<>();
+    private static LinkedList<String> list[];
+    private static int vertices;
 
-    private static void insertData(String data) {
-        list.add(data);
-    }
-    
-    private static void BFS() {
+    BFS(int vertices) { // Creating a new linked list
+        this.vertices = vertices;
+        list = new LinkedList[vertices];
 
+        for (int i = 0; i < vertices; ++i)
+            list[i] = new LinkedList<>();
     }
+
+    private static BreadthFirstSearch(String s) {
+        boolean visited[] = new boolean[vertices];
+
+        LinkedList<String> queue = new LinkedList<>();
+
+        visited[s] = true;
+        queue.add(s);
+
+    } 
 
     public static void main(String[] args) {
-        
+
     }
 }
