2292c6f345df6b8c10016d4357c5a39703273d8e
khal-drog0 <harshvardhanguleria@gmail.com>
2022-03-31 23:38:27 +0530

    Modified

--- PrimsAlgo.java
+++ PrimsAlgo.java
@@ -3,138 +3,177 @@ import java.util.LinkedList;
 import java.util.TreeSet;
 import java.util.Comparator;
 
-class Node {
-    protected static int destination;
-    protected static int weight;
-    
-    Node (int a, int b) {
-        this.destination = a;
-        this.weight = b;
-    }
-}
-
-class Graph {
-    protected static int V;
-    protected static LinkedList<Node>[] adj;
-
-    Graph(int e) {
-        this.V = e;
-        adj = new LinkedList[V];
-        for (int i = 0; i < V; ++i) 
-            adj[i]= new LinkedList<>();
-    }
-}
-
-// Class to represent a node in priority queue
-class Node1 {
-    protected static int vertex;
-    protected static int key;
-}
-
-// Comparator class for priority queue
-// returns 1 if node0.key > node1.key
-// returns 0 if node0.key < node1.key
-// returns -1 otherwise
-class comparator implements Comparator<Node1> {
-    
-    @Override
-    public int compare(Node1 node0, Node1 node1) {
-        return node0.key - node1.key;
-    }
-}
-
 public class PrimsAlgo {
-
-    // Adding edge between two vertices
-    private static void addEdge(Graph graph, int source, int destination,
-                                int weight) {
-        Node node0 = new Node(destination, weight);
-        Node node = new Node(source, weight);
-        graph.adj[source].addLast(node0);
-        graph.adj[destination].addLast(node);
+    class Node1 {
+ 
+        // Stores destination vertex in adjacency list
+        int dest;
+ 
+        // Stores weight of a vertex in the adjacency list
+        int weight;
+ 
+        // Constructor
+        Node1(int a, int b)
+        {
+            dest = a;
+            weight = b;
+        }
     }
-
-    // Finding the MST
-    private static void mst(Graph graph) {
-        
-        // Whether the vertex is a priority queue or not
-        Boolean[] mstSet = new Boolean[graph.V];
-        Node1[] e = new Node1[graph.V];
-
+    static class Graph {
+ 
+        // Number of vertices in the graph
+        int V;
+ 
+        // List of adjacent nodes of a given vertex
+        LinkedList<Node1>[] adj;
+ 
+        // Constructor
+        Graph(int e)
+        {
+            V = e;
+            adj = new LinkedList[V];
+            for (int o = 0; o < V; o++)
+                adj[o] = new LinkedList<>();
+        }
+    }
+ 
+    // class to represent a node in PriorityQueue
+    // Stores a vertex and its corresponding
+    // key value
+    class Node {
+        int vertex;
+        int key;
+    }
+ 
+    // Comparator class created for PriorityQueue
+    // returns 1 if node0.key > node1.key
+    // returns 0 if node0.key < node1.key and
+    // returns -1 otherwise
+    class comparator implements Comparator<Node> {
+ 
+        @Override
+        public int compare(Node node0, Node node1)
+        {
+            return node0.key - node1.key;
+        }
+    }
+ 
+    // method to add an edge
+    // between two vertices
+    void addEdge(Graph graph, int src, int dest, int weight)
+    {
+ 
+        Node1 node0 = new Node1(dest, weight);
+        Node1 node = new Node1(src, weight);
+        graph.adj[src].addLast(node0);
+        graph.adj[dest].addLast(node);
+    }
+ 
+    // method used to find the mst
+    void prims_mst(Graph graph)
+    {
+ 
+        // Whether a vertex is in PriorityQueue or not
+        Boolean[] mstset = new Boolean[graph.V];
+        Node[] e = new Node[graph.V];
+ 
         // Stores the parents of a vertex
         int[] parent = new int[graph.V];
-
-        for (int i = 0; i < graph.V; ++i)
-            e[i] = new Node1();
-
-        for (int i = 0; i < graph.V; ++i) {
-
+ 
+        for (int o = 0; o < graph.V; o++)
+            e[o] = new Node();
+ 
+        for (int o = 0; o < graph.V; o++) {
+ 
             // Initialize to false
-            mstSet[i] = false;
-
+            mstset[o] = false;
+ 
             // Initialize key values to infinity
-            e[i].key = Integer.MAX_VALUE;
-            e[i].vertex = i;
-            parent[i] = -1;
+            e[o].key = Integer.MAX_VALUE;
+            e[o].vertex = o;
+            parent[o] = -1;
         }
-
-        // Include the source vertex in mstSet
-        mstSet[0] = true;
-
+ 
+        // Include the source vertex in mstset
+        mstset[0] = true;
+ 
         // Set key value to 0
-        // so that it is extracted first out of
-        // Priority Queue
+        // so that it is extracted first
+        // out of PriorityQueue
         e[0].key = 0;
-
-        // use tree set instead of Priority queue as the remove 
-        // function
-        TreeSet<Node1> queue = new TreeSet<Node1>(new comparator());
-
-        for (int i = 0; i < graph.V; ++i) 
-            queue.add(e[i]);
-
-        // Loop until the queue is not empty
+ 
+        // Use TreeSet instead of PriorityQueue as the remove function of the PQ is O(n) in java.
+        TreeSet<Node> queue = new TreeSet<Node>(new comparator());
+ 
+        for (int o = 0; o < graph.V; o++)
+            queue.add(e[o]);
+ 
+        // Loops until the queue is not empty
         while (!queue.isEmpty()) {
-
+ 
             // Extracts a node with min key value
-            Node1 node0 = queue.pollFirst();
-
-            // Include that node to the mstSet
-            mstSet[node0.vertex] = true;
-
+            Node node0 = queue.pollFirst();
+ 
+            // Include that node into mstset
+            mstset[node0.vertex] = true;
+ 
             // For all adjacent vertex of the extracted vertex V
-            for (Node iterator : graph.adj[node0.vertex]) {
-
+            for (Node1 iterator : graph.adj[node0.vertex]) {
+ 
                 // If V is in queue
-                if (mstSet[iterator.destination] == false) {
-                    
-                    // If the key value of the adjacent vertex is more
-                    // than the extracted key, update the key value
-                    // of the adjacent vertexx.
-                    // To update first remove and add the updated
-                    // vertex
-                    if (e[iterator.destination].key > iterator.weight) {
-                        queue.remove(e[iterator.destination]);
-                        e[iterator.destination].key = iterator.weight;
-                        queue.add(e[iterator.destination]);
-                        parent[iterator.destination] = node0.vertex;
+                if (mstset[iterator.dest] == false) {
+                    // If the key value of the adjacent vertex is
+                    // more than the extracted key
+                    // update the key value of adjacent vertex
+                    // to update first remove and add the updated vertex
+                    if (e[iterator.dest].key > iterator.weight) {
+                        queue.remove(e[iterator.dest]);
+                        e[iterator.dest].key = iterator.weight;
+                        queue.add(e[iterator.dest]);
+                        parent[iterator.dest] = node0.vertex;
                     }
                 }
             }
         }
-
-        // Print the vertex pairs of MST
-        System.out.println("\nThe Minimum Spanning Tree is");
-        for (int i = 1; i < graph.V; ++i) 
-            System.out.println(parent[i] +" - "+ i);
+ 
+        // Prints the vertex pair of mst
+        for (int o = 1; o < graph.V; o++)
+            System.out.println(parent[o] + " "
+                               + "-"
+                               + " " + o);
     }
+ 
+    public static void main(String[] args)
+    {
+        /*
+        int V = 9;
+ 
+        Graph graph = new Graph(V);
+ 
+        PrimsAlgo e = new PrimsAlgo();
+ 
+        e.addEdge(graph, 0, 1, 4);
+        e.addEdge(graph, 0, 7, 8);
+        e.addEdge(graph, 1, 2, 8);
+        e.addEdge(graph, 1, 7, 11);
+        e.addEdge(graph, 2, 3, 7);
+        e.addEdge(graph, 2, 8, 2);
+        e.addEdge(graph, 2, 5, 4);
+        e.addEdge(graph, 3, 4, 9);
+        e.addEdge(graph, 3, 5, 14);
+        e.addEdge(graph, 4, 5, 10);
+        e.addEdge(graph, 5, 6, 2);
+        e.addEdge(graph, 6, 7, 1);
+        e.addEdge(graph, 6, 8, 6);
+        e.addEdge(graph, 7, 8, 7);
+ 
+        // Method invoked
+        e.prims_mst(graph);
+        */
 
-    public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
         int V;
         int source, destination, weight;
-        Boolean exit = false;
 
         System.out.print("\nEnter the number of vertices: ");
         V = sc.nextInt();
@@ -142,20 +181,28 @@ public class PrimsAlgo {
         // Creating the graph
         Graph graph = new Graph(V);
 
-        while (!exit) {
+        PrimsAlgo p = new PrimsAlgo();
+
+        while (true) {
             System.out.println("\nEnter -1 to stop.");
             System.out.print("\nEnter source: ");
             source = sc.nextInt();
-            System.out.print("\nEnter destination: ");
-            destination = sc.nextInt();
-            System.out.print("\nEnter weight: ");
-            weight = sc.nextInt();
-        
-            // calling the function to add the edges
-            addEdge(graph, source, destination, weight);
+            
+            if (source == -1)
+                break;
+            else {
+                System.out.print("\nEnter destination: ");
+                destination = sc.nextInt();
+                System.out.print("\nEnter weight: ");
+                weight = sc.nextInt();
+            
+                // calling the function to add the edges
+                p.addEdge(graph, source, destination, weight);
+            }
         }
+        sc.close();
 
         // Calling the MST function
-        mst(graph);
+        p.prims_mst(graph);
     }
 }
